7b5e0d192bf85a3c04dbe7aac79e96cf240279b0
Ahneb <benjaminha1996@gmail.com>
2023-03-04 08:56:14 -0800

    added css styling edited the loginform and signup forms   - removed buttons and added placeholder divs instead   - removed the container and row components added a title.js in case i need to move the title to login and signup forms added base layout of page in app.js

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -7,30 +7,29 @@ import Row from 'react-bootstrap/Row';
 
 function LoginForm() {
   return (
-    <Container>
-      <Row>
-        <Form className='w-50'>
-          <Form.Group className="mb-3" controlId="formBasicEmail">
-            <Form.Label>Email address</Form.Label>
-            <Form.Control type="email" placeholder="Enter email" />
+        <Form id='Form_Holder'>
+          <Form.Group className="" controlId="formBasicEmail">
+            {/* <Form.Label>Email address</Form.Label> */}
+            <Form.Control type="email" placeholder="Enter Email..." />
             <Form.Text className="text-muted">
-              We'll never share your email with anyone else.
+              {/* We'll never share your email with anyone else. */}
             </Form.Text>
           </Form.Group>
 
-          <Form.Group className="mb-3" controlId="formBasicPassword">
-            <Form.Label>Password</Form.Label>
-            <Form.Control type="password" placeholder="Password" />
+          <Form.Group className="" controlId="formBasicPassword">
+            {/* <Form.Label>Password</Form.Label> */}
+            <Form.Control type="password" placeholder="Enter Password..." />
           </Form.Group>
-          <Form.Group className="mb-3" controlId="formBasicCheckbox">
-            <Form.Check type="checkbox" label="Check me out" />
-          </Form.Group>
-          <Button variant="primary" type="submit">
+          {/* <Form.Group className="" controlId="formBasicCheckbox">
+            <Form.Check type="checkbox" label="Save Login" />
+          </Form.Group> */}
+          <div id="Login_Button" className="bg-brown flex-centered btn main-btn">
             Login
-          </Button>
+          </div>
+          <div id="Signup_Button" className="flex-centered btn sub-btn">
+            <a>Sign up</a>
+          </div>
         </Form>
-      </Row>
-    </Container>
   );
 }
 
