3972f6190ece4f68f66b0bd338ec898f2be84640
NSRehm <111025475+NSRehm@users.noreply.github.com>
2023-03-08 11:53:10 -0800

    create paystripecontent.js

---
+++ client/src/components/paystripecontent.js
@@ -0,0 +1,17 @@
+import { Elements } from "@stripe/react-stripe-js"
+import { loadStripe } from "@stripe/stripe-js"
+import React from "react"
+import PaymentForm from "./PaymentForm"
+
+const PUBLIC_KEY = "filler_key"
+
+const stripeTestPromise = loadStripe(PUBLIC_KEY)
+
+export default function StripeContainer() {
+	return (
+		<Elements stripe={stripeTestPromise}>
+			<PaymentForm />
+		</Elements>
+	)
+}
+
