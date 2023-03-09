ef86630a8f2e01b389781483069d0e7e467ac86e
vantam8300 <vantam8300@gmail.com>
2023-03-08 23:38:12 -0800

    remove confirm pasword

--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -54,71 +54,62 @@ function SignupForm() {
 
   return (
     <>
-    <section className='signup-intro'><p >Create account and start managing your tasks!</p></section>
-    <div className='signup-box'>
-      
-      <Form id='Form_Holder' onSubmit={handleFormSubmit}>
-        <Form.Group className="email_text" controlId="formBasicEmail">
-          <Form.Label htmlFor='email'>Email</Form.Label>
-          {/* <Form.Label>Email address</Form.Label> */}
-          <Form.Control 
-            name='email' 
-            type="email" 
-            placeholder="Enter Email..." 
-            onChange={handleInputChange}
-            value={userFormData.email}
-            required/>
-          <Form.Control.Feedback type='invalid'>Email is required!</Form.Control.Feedback>
-        </Form.Group>
+      <section className='signup-intro'><p >Create account and start managing your tasks!</p></section>
+      <div className='signup-box'>
 
-        <Form.Group className="user-text" controlId="formBasicUsername">
-          <Form.Label htmlFor='username'>Username</Form.Label>
-          {/* <Form.Label>Email address</Form.Label> */}
-          <Form.Control 
-            name='username'  
-            type='text' 
-            placeholder="Enter Username..." 
-            onChange={handleInputChange}
-            value={userFormData.username}
-            required/>
-          {/* <Form.Text className="text-muted">
+        <Form id='Form_Holder' onSubmit={handleFormSubmit}>
+          <Form.Group className="email_text" controlId="formBasicEmail">
+            <Form.Label htmlFor='email'>Email</Form.Label>
+            {/* <Form.Label>Email address</Form.Label> */}
+            <Form.Control
+              name='email'
+              type="email"
+              placeholder="Enter Email..."
+              onChange={handleInputChange}
+              value={userFormData.email}
+              required />
+            <Form.Control.Feedback type='invalid'>Email is required!</Form.Control.Feedback>
+          </Form.Group>
+
+          <Form.Group className="user-text" controlId="formBasicUsername">
+            <Form.Label htmlFor='username'>Username</Form.Label>
+            {/* <Form.Label>Email address</Form.Label> */}
+            <Form.Control
+              name='username'
+              type='text'
+              placeholder="Enter Username..."
+              onChange={handleInputChange}
+              value={userFormData.username}
+              required />
+            {/* <Form.Text className="text-muted">
           We'll never share your email with anyone else.
           </Form.Text> */}
-          <Form.Control.Feedback type='invalid'>Username is required!</Form.Control.Feedback>
-        </Form.Group>
+            <Form.Control.Feedback type='invalid'>Username is required!</Form.Control.Feedback>
+          </Form.Group>
 
-        <Form.Group className="password_text" controlId="formBasicPassword">
-          <Form.Label htmlFor='password'>Password</Form.Label>
-          {/* <Form.Label>Password</Form.Label> */}
-          <Form.Control 
-            name='password' 
-            type="password" 
-            placeholder="Enter Password..." 
-            onChange={handleInputChange}
-            value={userFormData.password}
-            required/>
+          <Form.Group className="password_text" controlId="formBasicPassword">
+            <Form.Label htmlFor='password'>Password</Form.Label>
+            <Form.Control
+              name='password'
+              type="password"
+              placeholder="Enter Password..."
+              onChange={handleInputChange}
+              value={userFormData.password}
+              required />
             <Form.Control.Feedback type='invalid'>Password is required!</Form.Control.Feedback>
-        </Form.Group>
+          </Form.Group>
 
-        <Form.Group className="confirm-text" controlId="formBasicPassword">
-          <Form.Label htmlFor='password'>Confirm Password</Form.Label>
-          {/* <Form.Label>Password</Form.Label> */}
-          <Form.Control name='confirmPassword' type="password" placeholder="Confirm Password..." />
-        </Form.Group>
-        {/* <Form.Group className="" controlId="formBasicCheckbox">
-        <Form.Check type="checkbox" label="Save Login" />
-      </Form.Group> */}
-         <Button
-          type='submit'
-          id="Signup_Button"
-          className="bg-brown flex-centered btn main-btn">
-          Sign up
-        </Button>
+          <Button
+            type='submit'
+            id="Signup_Button"
+            className="bg-brown flex-centered btn main-btn">
+            Sign up
+          </Button>
 
-        <Link as={Link} to='/login' id="Login_Button" className="flex-centered btn sub-btn">
-          Login
-        </Link>
-      </Form>
+          <Link as={Link} to='/login' id="Login_Button" className="flex-centered btn sub-btn">
+            Login
+          </Link>
+        </Form>
       </div>
     </>
 
